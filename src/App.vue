08bd7d9ca4b4290f6070eaae6c255a8b4db853ae
Antonio Đurčević <antonio.durcevic@gmail.com>
2022-09-29 22:30:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheHeader from "./components/layout/TheHeader.vue";
+import TheSection from "./components/layout/TheSection.vue";
+</script>
+
+<template>
+  <the-header></the-header>
+  <the-section></the-section>
+</template>
+
+<style></style>
---
+++ src/components/layout/TheSection.vue
@@ -0,0 +1,19 @@
+<script></script>
+<template>
+  <main class="main">
+    <section class="section">
+      <div class="section__inner">
+        <slot></slot>
+        <slot name="countries"></slot>
+      </div>
+    </section>
+  </main>
+</template>
+<style lang="css">
+.section {
+  background-color: var(--background-primary);
+  width: 100%;
+  height: 500px;
+  box-shadow: 0 -1px 2px 1px var(--border-color);
+}
+</style>
